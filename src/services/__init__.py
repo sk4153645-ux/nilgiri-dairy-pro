0ae715ffd5c8ad2227c4968dc7f03f8db33f45bd
sk4153645-ux <sk4153645@gmail.com>
2026-09-04 08:32:11 +0530

    Update __init__.py

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,8 +1,4 @@
-"""
-Services Module
-
-Contains all business logic separated from UI.
-"""
+"""Services layer entry point."""
 
 from src.services.auth_service import AuthService
 from src.services.dairy_service import DairyService
@@ -10,6 +6,8 @@ from src.services.ledger_service import LedgerService
 from src.services.report_service import ReportService
 from src.services.notification_service import NotificationService
 from src.services.backup_service import BackupService
+from src.services.audit_service import AuditService
+from src.services.offline_sync_service import OfflineSyncService
 
 __all__ = [
     "AuthService",
@@ -18,4 +16,6 @@ __all__ = [
     "ReportService",
     "NotificationService",
     "BackupService",
+    "AuditService",
+    "OfflineSyncService",
 ]
